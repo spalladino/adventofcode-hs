d571552d18024b8ce714de10d06125970fab2331
Santiago Palladino <spalladino@gmail.com>
2019-12-06 15:33:49 -0300

    Day 02 p2

--- src/Day02.hs
+++ src/Day02.hs
@@ -8,14 +8,26 @@ import Utils.Data
 import Data.List.Split
 
 -- |https://adventofcode.com/2019/day/2
+day02p1 :: IO ()
+day02p1 = interact $ show . head . intcode . (setupProgram 12 2) . (map (\x -> read x :: Integer)) . (splitOn ",")
+
 day02 :: IO ()
-day02 = interact $ show . head . intcode . setupProgram . (map (\x -> read x :: Integer)) . (splitOn ",")
+day02 = interact $ show . encodeInput . findInput 19690720 . (map (\x -> read x :: Integer)) . (splitOn ",")
 
 type Program = [Integer]
 type IP = Int
+type Noun = Integer
+type Verb = Integer
 
-setupProgram :: Program -> Program
-setupProgram = assocIn 1 12 . assocIn 2 2
+encodeInput :: (Noun, Verb) -> Integer
+encodeInput (n, v) = 100 * n + v
+
+findInput :: Integer -> Program -> (Noun, Verb)
+findInput target p = snd $ head $ dropWhile ((/= target) . fst) search
+      where search = [(head $ intcode $ setupProgram n v p, (n, v)) | n <- [0..1000], v <- [0..1000]]
+
+setupProgram :: Noun -> Verb -> Program -> Program
+setupProgram n v = assocIn 1 n . assocIn 2 v
 
 intcode :: Program -> Program
 intcode p = intcodeIP p 0
